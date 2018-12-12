3bf1f75bf4b51b1afef02ce20f9b0c41c0d55d12
nabtron <developer@nabtron.com>
2018-12-13 00:32:43 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,6 +15,36 @@ function schema_lite_child_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'schema_lite_child_enqueue_styles' );
 
+function schema_lite_child_override_scripts()
+{
+    wp_dequeue_script( 'schema-lite-customscripts' );
+    wp_enqueue_script( 'schema-lite-child-customscripts', get_stylesheet_directory_uri() . '/js/customscripts.js', false, wp_get_theme()->get('Version'));
+}
+add_action( 'wp_enqueue_scripts', 'schema_lite_child_override_scripts', 11 );
+
+
+/* remove jquery migrate */
+ function remove_jquery_migrate( $scripts ) {
+ 	if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+ 		$script = $scripts->registered['jquery'];
+ 
+ 		if ( $script->deps ) { // Check whether the script has any dependencies
+ 				$script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+ 		}
+ 	}
+ }
+ 
+ add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+ 
+/* Remove type tag from script and style */
+add_filter('style_loader_tag', 'codeless_remove_type_attr', 10, 2);
+add_filter('script_loader_tag', 'codeless_remove_type_attr', 10, 2);
+add_filter('autoptimize_html_after_minify', 'codeless_remove_type_attr', 10, 2);
+function codeless_remove_type_attr($tag, $handle='')
+{
+    return preg_replace("/type=['\"]text\/(javascript|css)['\"]/", '', $tag);
+}
+
 /**
  * Post Layout for Archives - override from parent theme
  */
